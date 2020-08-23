3736491f6a4136af91a85a3451759485591e571d
Sam Wyness <samwyness22@gmail.com>
2020-08-23 17:31:58 +1000

    cleanup chart story

--- src/components/Chart/Chart.stories.tsx
+++ src/components/Chart/Chart.stories.tsx
@@ -1,79 +1,40 @@
 import React from 'react';
 
 import { colors } from '../../utils/colors';
-
 import { Chart } from './Chart';
-import { BarVertical } from '../BarVertical';
-import { PieChart } from '../PieChart';
 
-export type ChartProps = {
-  data: any;
-  config: any;
-};
+const data = [
+  ['Financial Year', 'Net Income'],
+  ['2015/2016', 2698, { color: colors.green_300 }],
+  ['2016/2017', 44438, { color: colors.green_300 }],
+  ['2017/2018', 19639, { color: colors.green_300 }],
+  ['2018/2019', 22914, { color: colors.green_300 }],
+  ['2019/2020', 86409, { color: colors.green_300 }],
+  ['2020/2021', 13526, { color: colors.green_300 }],
+];
 
 export default {
-  title: 'Components / Chart',
+  title: 'Components / Charts',
   component: Chart,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 };
 
-const Template = (args: ChartProps) => {
-  let ChartType;
+const Template = (args) => <Chart {...args} />;
 
-  switch (args.config.type) {
-    case 'bar-vertical':
-      ChartType = BarVertical;
-      break;
-
-    case 'pie':
-    case 'donut':
-      ChartType = PieChart;
-      break;
-
-    default:
-      ChartType = BarVertical;
-      break;
-  }
-
-  return (
-    <div className={`chart-container chart-type-${args.config.type}`}>
-      <div className="chart-title">
-        <strong>{args.config.title}</strong>
-      </div>
-
-      <ChartType {...args} />
-    </div>
-  );
+export const BarVertical = Template.bind({});
+BarVertical.args = {
+  type: 'bar-vertical',
+  title: 'Income',
+  valuePrefix: '$',
+  data,
 };
 
-export const BarVerticalExample = Template.bind({});
-BarVerticalExample.args = {
-  data: [
-    ['Task Status', 'Tasks'],
-    ['Open', 100, { color: colors.deep_purple_300 }],
-    ['In Progress', 100, { color: colors.blue_300 }],
-    ['Review', 100, { color: colors.red_300 }],
-    ['Complete', 100, { color: colors.green_300 }],
-  ],
-  config: {
-    type: 'bar-vertical',
-    title: 'Project Tasks',
-  },
-};
-
-export const PieChartExample = Template.bind({});
-PieChartExample.args = {
-  data: [
-    ['Task Status', 'Tasks'],
-    ['Open', 100, { color: colors.deep_purple_300 }],
-    ['In Progress', 100, { color: colors.blue_300 }],
-    ['Review', 100, { color: colors.red_300 }],
-    ['Complete', 100, { color: colors.green_300 }],
-  ],
-  config: {
-    type: 'donut',
-    title: 'Project Tasks',
-  },
+export const PieChart = Template.bind({});
+PieChart.args = {
+  type: 'donut',
+  title: 'Income',
+  valuePrefix: '$',
+  data,
 };
